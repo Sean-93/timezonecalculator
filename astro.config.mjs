340fbba638868ec054a053debf761562ce84386c
unknown <seandanielrooney@gmail.com>
2025-04-14 12:23:20 -0700

    time to release it right this time with a better domain and a robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://timezonecalculator.app/",
+  site: "https://tzcompare.com/",
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
